feec1a0101d74f076b9803817653126b0492ef50
Adam Ismail <adam_ismaail98@outlook.com>
2024-03-26 22:58:52 +0100

    created loginpage komponent and added routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
+import App from "./App";
+import LoginPage from "./pages/LoginPage";
 
-const router = createBrowserRouter([]);
+const router = createBrowserRouter([
+  { path: "/", element: <App /> },
+  { path: "/login", element: <LoginPage /> },
+]);
 
 export default router;
