0e266337f8e4619761008a596a21b59d5b5f2b4b
Emil <emil.walfridsson@icloud.com>
2024-03-26 17:52:14 +0100

    Added Links and search input

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,39 @@
+import { Link } from "react-router-dom";
+
 function NavBar() {
-  return <h1>Navbar</h1>;
+  return (
+    <div className="p-4">
+      <div className="navbar bg-base-100 shadow rounded-lg">
+        <div className="navbar-start">
+          <Link to="/" className="btn btn-ghost text-xl ">
+            Home
+          </Link>
+          <Link to="/" className="btn btn-ghost text-xl ">
+            Login
+          </Link>
+
+          <Link to="/" className="btn btn-ghost text-xl ">
+            Register
+          </Link>
+          <Link to="/" className="btn btn-ghost text-xl ">
+            Profile
+          </Link>
+        </div>
+        <div className="navbar-center">
+          <h1 className="text-3xl">Intensive-Trading</h1>
+        </div>
+        <div className="navbar-end">
+          <div className="form-control">
+            <input
+              type="text"
+              placeholder="Search"
+              className="input input-bordered w-24 md:w-auto"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default NavBar;
