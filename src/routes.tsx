9a42a55b7bec8cf805fde32d6eaf7247d72b7158
Emil <emil.walfridsson@icloud.com>
2024-03-27 20:10:33 +0100

    created profilepage

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,11 +2,15 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 
 import RegisterPage from "./pages/RegisterPage";
+import ProfilePage from "./pages/ProfilePage";
 
 const router = createBrowserRouter([
-  { path: "/", element: <App /> },
+  {
+    path: "/",
+    element: <App />,
+    children: [{ path: "profile", element: <ProfilePage /> }],
+  },
   { path: "/registerpage", element: <RegisterPage /> },
-
 ]);
 
 export default router;
