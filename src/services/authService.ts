8b8037fff3d1fb7cb57477d1573bb2beda8f53d6
armen <armenbakir@hotmail.com>
2024-03-31 20:25:53 +0200

    made users to be able to be created

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -3,7 +3,8 @@ import axios from "axios";
 import { jwtDecode } from "jwt-decode";
 
 const TOKEN_KEY = "token";
-const API_BASEURL = "http://localhost:9111/api/auth";
+const API_BASEURL = "http://localhost:5570/api/auth";
+//const CREDENTIALS = "?username=armen&accessCode=gdhHaS";
 
 axios.defaults.headers.common["x-auth-token"] = getJwt();
 
