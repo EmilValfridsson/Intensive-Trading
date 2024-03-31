213e8348b8dbd908d98d71b93bc6619f7349a8dc
armen <armenbakir@hotmail.com>
2024-03-31 20:25:02 +0200

    added childer paths

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,14 +2,18 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import LoginPage from "./pages/LoginPage";
 import Logout from "./components/Logout";
-
+import RegisterPage from "./pages/RegisterPage";
 
 const router = createBrowserRouter([
-  { path: "/", element: <App /> },
-  { path: "/registerpage", element: <RegisterPage /> },
-  { path: "/login", element: <LoginPage /> },
-  { path: "/logout", element: <Logout /> },
-
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/registerpage", element: <RegisterPage /> },
+      { path: "/login", element: <LoginPage /> },
+      { path: "/logout", element: <Logout /> },
+    ],
+  },
 ]);
 
 export default router;
