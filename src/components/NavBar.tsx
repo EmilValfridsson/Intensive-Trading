3bad60d44f7f64833b437769b91818a223159db7
armen <armenbakir@hotmail.com>
2024-04-02 17:16:25 +0200

    added auto login and register

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,22 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import authService from "../services/authService";
+
+export interface User {
+  id: string;
+  name: string;
+  username: string;
+  isAdmin: boolean;
+}
 
 function NavBar() {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const user = authService.getCurrentUser();
+
+    setUser(user);
+  }, []);
   return (
     <div className="p-4">
       <div className="navbar bg-base-100 shadow rounded-lg">
@@ -8,16 +24,34 @@ function NavBar() {
           <Link to="/" className="btn btn-ghost text-xl ">
             Home
           </Link>
-          <Link to="/login" className="btn btn-ghost text-xl ">
-            Login
-          </Link>
-
-          <Link to="/registerpage" className="btn btn-ghost text-xl ">
-            Register
-          </Link>
-          <Link to="/" className="btn btn-ghost text-xl ">
-            Profile
-          </Link>
+          {user && (
+            <>
+              <li className="nav-item">
+                <Link className="btn btn-ghost text-xl " to="/profile">
+                  {user.name}
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="btn btn-ghost text-xl " to="/logout">
+                  Logout
+                </Link>
+              </li>
+            </>
+          )}
+          {!user && (
+            <>
+              <li className="nav-item">
+                <Link className="btn btn-ghost text-xl " to="/login">
+                  Login
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="btn btn-ghost text-xl " to="/registerpage">
+                  Register
+                </Link>
+              </li>
+            </>
+          )}
         </div>
         <div className="navbar-center">
           <h1 className="text-3xl">Intensive-Trading</h1>
