4edb5ef795917415070d6edf4066c440d84bd3cd
Emil <emil.walfridsson@icloud.com>
2024-04-02 18:12:14 +0200

    import registerpage in routes.tsx

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,14 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import LoginPage from "./pages/LoginPage";
 import Logout from "./components/Logout";
-
+import RegisterPage from "./pages/RegisterPage";
 
 const router = createBrowserRouter([
   { path: "/", element: <App /> },
   { path: "/registerpage", element: <RegisterPage /> },
   { path: "/login", element: <LoginPage /> },
   { path: "/logout", element: <Logout /> },
-
 ]);
 
 export default router;
