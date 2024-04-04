ff9ca575118a343299ed56c22a71cf41da9ae20a
Emil <emil.walfridsson@icloud.com>
2024-04-04 21:03:46 +0200

    Created userprovider

--- src/UserContext.tsx
+++ src/UserContext.tsx
@@ -1,7 +1,46 @@
-import { PropsWithChildren, createContext, useState } from "react";
+import {
+  PropsWithChildren,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import authService from "./services/authService";
+import { User } from "./types";
 
-const UserContext = createContext(null);
+interface UserContextType {
+  user: User | null;
+  setUser: React.Dispatch<React.SetStateAction<User | null>>;
+}
 
+const defaultUserContext: UserContextType = {
+  user: null,
+  setUser: () => null,
+};
+const UserContext = createContext<UserContextType>(defaultUserContext);
 export default function UserProvider({ children }: PropsWithChildren) {
-  const [user, setUser] = useState();
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const currentUser = await authService.getCurrentUser();
+        setUser(currentUser);
+      } catch (error) {
+        console.error("Error fetching user:", error);
+        setUser(null);
+      }
+    };
+
+    fetchUser();
+  }, []);
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+}
+
+export function useUserContext() {
+  return useContext(UserContext);
 }
