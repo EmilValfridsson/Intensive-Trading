625ffab78ccfb3c4340c3e2d7fcec184e0bbea1d
armen <armenbakir@hotmail.com>
2024-04-09 12:14:10 +0200

    Made searchbar useable

--- src/components/Stock.tsx
+++ src/components/Stock.tsx
@@ -1,28 +1,33 @@
 import { useEffect, useState } from "react";
 import Plot from "react-plotly.js";
 
-function Stock(value: string) {
+interface StockProps {
+  searchValue: string;
+}
+function Stock({ searchValue }: StockProps) {
   const [xValues, setXValues] = useState<string[]>([]);
   const [yValues, setYValues] = useState<number[]>([]);
   useEffect(() => {
-    setXValues([]);
-    setYValues([]);
-    fetch(`http://localhost:9111/api/stocks?data=${value}`)
-      .then((res) => res.json())
-      .then((data) => {
-        const newXValues: string[] = [];
-        const newYValues: number[] = [];
-        for (var key in data[`Time Series (Daily)`]) {
-          newXValues.push(key);
-          newYValues.push(data[`Time Series (Daily)`][key][`1. open`]);
-        }
-        setXValues((prevXValues) => [...prevXValues, ...newXValues]);
-        setYValues((prevYValues) => [...prevYValues, ...newYValues]);
-      });
-  }, [value]);
+    if (searchValue) {
+      setXValues([]);
+      setYValues([]);
+      fetch(`http://localhost:9111/api/stocks?data=${searchValue}`)
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          const newXValues: string[] = [];
+          const newYValues: number[] = [];
+          for (var key in data[`Time Series (Daily)`]) {
+            newXValues.push(key);
+            newYValues.push(data[`Time Series (Daily)`][key][`1. open`]);
+          }
+          setXValues((prevXValues) => [...prevXValues, ...newXValues]);
+          setYValues((prevYValues) => [...prevYValues, ...newYValues]);
+        });
+    }
+  }, [searchValue]);
   return (
     <div>
-      <h1>Stock</h1>
       <Plot
         data={[
           {
@@ -33,7 +38,7 @@ function Stock(value: string) {
             marker: { color: "green" },
           },
         ]}
-        layout={{ width: 640, height: 480, title: `${value}` }}
+        layout={{ width: 640, height: 480, title: `${searchValue}` }}
       />
     </div>
   );
